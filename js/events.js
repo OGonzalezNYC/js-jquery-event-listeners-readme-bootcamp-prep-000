227b237186d98a3fe58329ea4464a354b1030bea
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-09-11 20:57:32 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -4,17 +4,12 @@ function getIt() {
   });
 }
 
-//Define a function frameIt that does not accept a parameter. The function should bind the load event that adds the class tasty to the image to add a red frame to the image.
 function frameIt() {
   $("img").addClass("tasty")
   //$("img").on("load", function() {img.addClass="tasty"})
 }
 
-//function frameIt() {
-//  $('img').on('load', function() {
-//    $('img').addClass('tasty')
-//  })
-//}
+
 function pressIt() {
   $("#typing").on("keydown", function(key) {
     if(key.which === 71) {
@@ -37,4 +32,4 @@ frameIt
 pressIt
 submitIt
 
-});
+})
